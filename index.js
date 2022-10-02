6a866a28c7415e5b1394e02aa4ba12fa3d8ab536
Aminul Islam Tibro <aminultibro015@gmail.com>
2022-10-02 20:49:39 +0600

    Project

---
+++ index.js
@@ -0,0 +1,50 @@
+let toggle = false;
+let inputFiled = document.querySelector('.serach-input');
+document.querySelector('.search-icon').addEventListener('click',(e)=>
+{
+    if(toggle === false)
+    {
+        console.log("Hello");
+        inputFiled.classList.add("search-input-filed");
+        toggle = true;
+    }
+    else if(toggle === true)
+    {
+        console.log("hi");
+        inputFiled.classList.remove('search-input-filed');
+        toggle = false;
+        
+       
+    }
+   
+})
+ 
+var typed = new Typed('.banner-title', {
+    strings: ['Company slogan here'],
+    typeSpeed:100,
+    backSpeed : 100,
+    loop: true,
+    smartBackspace: true,
+    cursor:"|"
+  });
+ 
+$('.count-up-1').counterUp
+({
+    delay: 10,
+    time: 4000
+})
+$('.count-up-2').counterUp
+({
+    delay: 10,
+    time: 8000
+})
+$('.count-up-3').counterUp
+({
+    delay: 10,
+    time: 6000
+})
+$('.count-up-4').counterUp
+({
+    delay: 10,
+    time: 4000
+})
